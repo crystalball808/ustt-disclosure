cab854df491eb2a1c6e014e9af11defdeb81b5d9
drizzer14 <drizzer14@gmail.com>
2021-02-09 11:35:17 +0200

    Initial commit

---
+++ src/app/app.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Disclosure } from '../disclosure';
+
+export const App = () => {
+	return <Disclosure />;
+};
---
+++ src/disclosure/disclosure.component.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Disclosure = () => {
+	return <p>Disclosure</p>;
+};
