b2a23817b40c09d3935e36fa8cf301ab1dc07d82
Roman <romagritcaenko@gmail.com>
2021-02-15 16:03:09 +0200

    done

--- src/app/app.component.jsx
+++ src/app/app.component.jsx
@@ -2,6 +2,36 @@ import React from 'react';
 
 import { Disclosure } from '../disclosure';
 
+const test1 = `Governments around the world spend an estimated US$9.5 trillion through contracts every year. 
+Yet, contracting information is often unavailable for public scrutiny.
+
+The Open Contracting Data Standard (OCDS) enables disclosure of data and documents at all stages of the contracting process 
+by defining a common data model. It was created to support organizations to increase contracting transparency, 
+and allow deeper analysis of contracting data by a wide range of users.
+vxcvxcvxcvxcvxcvxcv`
+
+const test2 = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas necessitatibus nisi vel velit, laborum molestias at numquam debitis aliquam nostrum repellendus recusandae illo officia omnis quis quia quaerat voluptates optio.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas necessitatibus nisi vel velit, laborum molestias at numquam debitis aliquam nostrum repellendus recusandae illo officia omnis quis quia quaerat voluptates optio.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas necessitatibus nisi vel velit, laborum molestias at numquam debitis aliquam nostrum repellendus recusandae illo officia omnis quis quia quaerat voluptates optio.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas necessitatibus nisi vel velit, laborum molestias at numquam debitis aliquam nostrum repellendus recusandae illo officia omnis quis quia quaerat voluptates optio.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas necessitatibus nisi vel velit, laborum molestias at numquam debitis aliquam nostrum repellendus recusandae illo officia omnis quis quia quaerat voluptates optio.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas necessitatibus nisi vel velit, laborum molestias at numquam debitis aliquam nostrum repellendus recusandae illo officia omnis quis quia quaerat voluptates optio.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas necessitatibus nisi vel velit, laborum molestias at numquam debitis aliquam nostrum repellendus recusandae illo officia omnis quis quia quaerat voluptates optio.`
+
+
 export const App = () => {
-	return <Disclosure />;
+	return (
+		<>
+		<Disclosure summary="Open Contracting Data Standard">
+			<p>
+				{test1}
+			</p>
+		</Disclosure>
+		<Disclosure summary="Lorem">
+			<p>
+				{test2}
+			</p>
+		</Disclosure>
+		</>
+	);
 };
